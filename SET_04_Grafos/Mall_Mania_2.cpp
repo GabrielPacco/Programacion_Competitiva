d0f480eacfad0db148e859db0a688b1ef441e907
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-11-12 03:35:01 -0500

    Mall_mania

---
+++ SET_04_Grafos/Mall_Mania_2.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 1 << 12;
+
+int grid[MAX][MAX];
+vector<pair<int, int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int main(int, char**) {
+  ios::sync_with_stdio(NULL);
+  cin.tie(NULL);
+
+  int c_co, avenid, street;
+  while (cin >> c_co, c_co) {
+    memset(grid, -1, sizeof(grid));
+
+    queue<pair<int, int>> q_exe;
+    while (c_co--) {
+      cin >> avenid >> street;
+      grid[avenid][street] = 0;
+      q_exe.push({avenid, street});
+    }
+
+    cin >> c_co;
+    while (c_co--) {
+      cin >> avenid >> street;
+      grid[avenid][street] = -2;
+    }
+
+    int distance = 0;
+    bool there_is_path = false;
+    while (!q_exe.empty() && !there_is_path) {
+      auto u = q_exe.front();
+      q_exe.pop();
+
+      for (auto direction : directions) {
+        pair<int, int> v = {u.first + direction.first, u.second + direction.second};
+        
+        if (0 <= v.first < MAX && 0 <= v.second < MAX) {
+          if (grid[v.first][v.second] == -1) {
+            grid[v.first][v.second] = grid[u.first][u.second] + 1;
+            q_exe.push(v);
+          }
+          else if (grid[v.first][v.second] == -2) {
+            distance = grid[u.first][u.second] + 1;
+            there_is_path = true;
+          }
+        }
+      }
+    }
+
+    cout << distance << '\n';
+
+  }
+
+  return 0;
+}
--- SET_04_Grafos/mall_mania.cpp
+++ SET_04_Grafos/mall_mania.cpp
@@ -5,53 +5,55 @@
 
 using namespace std;
 
-const int EMPTY = -1;  // empty cell
-const int GOAL = -2;   // goal cell
-const int MAXSIZE = 2010;  // maximum size of the maze
-vector<vector<int>> grid;   // the maze
+// Mall Mania
+
+const int EMPTY = -1;  // nodo vacío
+const int GOAL = -2;   // nodo objetivo
+const int MAXSIZE = 2010;  // tamaño máximo del laberinto
+vector<vector<int>> grid;   // el laberinto
 
 vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};  // directions
 
 int main() {
-    int p;  // number of test cases
-    while (cin >> p, p) {  // for each test case
-        grid.assign(MAXSIZE, vector<int>(MAXSIZE, EMPTY)); // initialize the maze
+    int p;  // número de casos de prueba
+    while (cin >> p, p) {  // para cada caso de prueba
+        grid.assign(MAXSIZE, vector<int>(MAXSIZE, EMPTY)); // inicializar el laberinto ¿?
 
-        int a, s;  // number of rows and columns
-        queue<pair<int, int>> q;  // queue of cells to be processed
-        while (p--) {  // for each person
-            cin >> a >> s; // read the number of rows and columns
-            grid[a][s] = 0; // mark the cell as visited
-            q.push({a, s}); // add the cell to the queue
+        int a, s;  // número de filas y columnas
+        queue<pair<int, int>> q;  // cola de celdas a procesar
+        while (p--) {  // para cada persona
+            cin >> a >> s; // leer el número de filas y columnas
+            grid[a][s] = 0; // marcar la celda como visitada
+            q.push({a, s}); // añadir la celda a la cola
         }
-        cin >> p; // read the number of goals
-        while (p--) { // for each goal
-            cin >> a >> s; // read the coordinates of the goal
-            grid[a][s] = GOAL;  // mark the cell as goal
+        cin >> p; // leer el número de objetivos
+        while (p--) { // para cada objetivo
+            cin >> a >> s; // leer las coordenadas de la meta
+            grid[a][s] = GOAL;  // marcar la celda como objetivo
         }
 
-        int distance;   // distance from the starting cell
-        bool found_path = false;  // flag to indicate if a path was found
-        while (!q.empty() && !found_path) {  // while the queue is not empty and no path was found
-            pair<int, int> u = q.front();  // get the first cell in the queue
-            q.pop();  // remove the cell from the queue
+        int distance;   // distancia de la célula inicial
+        bool found_path = false;  // para indicar si se ha encontrado una ruta
+        while (!q.empty() && !found_path) {  // mientras la cola no esté vacía y no se haya encontrado ninguna ruta
+            pair<int, int> u = q.front();  // obtener la primera celda de la cola
+            q.pop();  // eliminar la célula de la cola
 
-            for (auto&& d : dirs) {  // for each direction
-                pair<int, int> v = {u.first + d.first, u.second + d.second};  // get the next cell
+            for (auto&& d : dirs) {  // para cada dirección
+                pair<int, int> v = {u.first + d.first, u.second + d.second};  // obtener la siguiente celda
 
-                if (0 <= v.first && v.first < MAXSIZE &&   // if the cell is inside the maze
-                    0 <= v.second && v.second < MAXSIZE) {  // and
-                        if (grid[v.first][v.second] == EMPTY) {  // if the cell is empty
-                            grid[v.first][v.second] = grid[u.first][u.second] + 1;  // mark the cell as visited
-                            q.push(v);     // add the cell to the queue
-                        } else if (grid[v.first][v.second] == GOAL) {  // if the cell is a goal
-                            distance = grid[u.first][u.second] + 1;  // get the distance
-                            found_path = true;    // mark that a path was found
+                //¿? 
+                if (0 <= v.first && v.first < MAXSIZE &&   // si la celda está dentro del laberinto
+                    0 <= v.second && v.second < MAXSIZE) {  // y no ha sido visitada
+                        if (grid[v.first][v.second] == EMPTY) {  // si la celda está vacía
+                            grid[v.first][v.second] = grid[u.first][u.second] + 1;  // marcar la celda como visitada
+                            q.push(v);     // añadir la celda a la cola
+                        } else if (grid[v.first][v.second] == GOAL) {  // si la celda es un objetivo
+                            distance = grid[u.first][u.second] + 1;  // obtener la distancia
+                            found_path = true;    // marcar que se ha encontrado una ruta
                         }
                     }
             }
         }
-
-        cout << distance << endl;  // print the distance
+        cout << distance << endl;  // imprimir la distancia
     }
 }
